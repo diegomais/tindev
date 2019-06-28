cd4f9b485ea5f3e2662e863e3851090254675840
Diego Mais <diegomais@live.com>
2019-06-28 00:05:55 -0300

    Add user update
    - This feature provides user to update account information.
    
    Changes to be committed:
    	modified:   src/app/controllers/UserController.js

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -20,7 +20,29 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ user: req.userId });
+    const user = await User.findByPk(req.userId);
+
+    const { email, oldPassword } = req.body;
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({
+          error: 'An account already exists with this email address.',
+        });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        error: 'Invalid password. Please try again.',
+      });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 }
 
