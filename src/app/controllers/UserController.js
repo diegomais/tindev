e733ce079021ef486f000cfdfa5aea7d13126942
Diego Mais <diegomais@live.com>
2019-06-27 22:34:53 -0300

    Add feature create user
    - This feature provides user to create a new user in database.
    
    Changes to be committed:
    	new file:   src/app/controllers/UserController.js
    	modified:   src/routes.js

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,21 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    // Check if email already exists in user database
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    // If email already exists in database return error message
+    if (userExists) {
+      return res.status(400).json({ error: 'This email already exists' });
+    }
+
+    // Else create user in database
+    const { id, name, email, provider } = await User.create(req.body);
+
+    // Then return user information
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/', async (req, res) => {
-  const { name, email, password_hash } = req.body;
-  const user = await User.create({
-    name,
-    email,
-    password_hash,
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
