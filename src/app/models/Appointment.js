79f87175a47f63772318b13a0d1533c5cdadc85e
Diego Mais <diegomais@live.com>
2019-07-01 23:11:03 -0300

    Add Appointment model
    - This model provide access to appointments table in database.
    Changes to be committed:
    	new file:   src/app/models/Appointment.js

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
