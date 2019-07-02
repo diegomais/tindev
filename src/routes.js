98e4f55da5da138fa1ad4192c61233c315e9d623
Diego Mais <diegomais@live.com>
2019-07-01 23:36:25 -0300

    Add validate and store appointment
    - This feature provides user to create an appointment with a provider.
    Changes to be committed:
    	modified:   package.json
    	modified:   yarn.lock
    	new file:   src/app/controllers/AppointmentController.js
    	modified:   src/routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -21,6 +22,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
