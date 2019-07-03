762f42bfb423bdbcada7685c04f12e4736977ad9
Diego Mais <diegomais@live.com>
2019-07-03 00:16:33 -0300

    Feature/Update notification as read
    - This feature provides user to update notification as read.
    Changes to be committed:
    	modified:   src/app/controllers/NotificationController.js
    	modified:   src/routes.js

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -18,6 +18,15 @@ class NotificationController {
       .limit(20);
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+    return res.json(notification);
+  }
 }
 
 export default new NotificationController();
