5af9551a8f244a8cbad1ca36c821f8f018e16f4f
Diego Mais <diegomais@live.com>
2019-07-02 22:19:37 -0300

    Add get appointments from authenticated user
    - This feature provides user to list their appointments.
    Changes to be committed:
    	modified:   src/app/controllers/AppointmentController.js
    	modified:   src/routes.js

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,8 +3,31 @@ import { isBefore, parseISO, startOfHour } from 'date-fns';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       date: Yup.date().required(),
