56b8a7fb0e8cff1adb742b8df16c01fe2168d33a
Diego Mais <diegomais@live.com>
2019-07-02 23:09:15 -0300

    Feature/Add get schedule of provider
    - This feature provides provider to list their schedule by date.
    Changes to be committed:
    	new file:   src/app/controllers/ScheduleController.js
    	modified:   src/routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
@@ -25,6 +26,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
