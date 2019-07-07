13e5abad0c6f9e4643d88c2ca9adafb0b80647b2
Diego Mais <diegomais@live.com>
2019-07-07 00:07:06 -0300

    Add virtual fields in Appointments model
    - This improvements show if a appointment is past and is cancellable.
    
    On branch chore/virtual-fields-to-appointments
    Changes to be committed:
    	modified:   src/app/controllers/AppointmentController.js
    	modified:   src/app/models/Appointment.js

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancellable'],
       limit: appointmentsPerPage,
       offset: (page - 1) * appointmentsPerPage,
       include: [
