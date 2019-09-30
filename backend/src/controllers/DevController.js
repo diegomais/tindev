06a3bc3615a42c94391cd65af7e748c3e34eb83e
Diego Mais <diegomais@live.com>
2019-09-30 23:35:01 +0200

    Add index on dev controller
    - This feature provides user with a list of new devs.
    
    On branch master
    Changes to be committed:
    	modified:   backend/src/controllers/DevController.js
    	modified:   backend/src/routes.js

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,22 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } },
+      ],
+    });
+
+    return res.json(users);
+  },
+
   async store(req, res) {
     const { username } = req.body;
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
 routes.post('/devs/:devId/dislikes', DislikeController.store);
