4abe701adddf5e9f9851fab4a2227ef43cdac2b6
Diego Mais <diegomais@live.com>
2019-10-13 19:26:40 +0200

    Add empty message
    - This feature gibes user a message if no dev is listed.
    
    On branch master
    Changes to be committed:
    	modified:   frontend/src/pages/Main.css
    	modified:   frontend/src/pages/Main.js

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -43,25 +43,29 @@ export default function Main({ match }) {
   return (
     <div className="main-container">
       <img src={logo} alt="TinDev" />
-      <ul>
-        {users.map(user => (
-          <li key={user._id}>
-            <img src={user.avatar} alt={user.name} />
-            <footer>
-              <strong>{user.name}</strong>
-              <p>{user.bio}</p>
-            </footer>
-            <div className="buttons">
-              <button type="button" onClick={() => handleDislike(user._id)}>
-                <img src={dislike} alt="Dislike" />
-              </button>
-              <button type="button" onClick={() => handleLike(user._id)}>
-                <img src={like} alt="Like" />
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name} />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="Like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">Empty :(</div>
+      )}
     </div>
   );
 }
