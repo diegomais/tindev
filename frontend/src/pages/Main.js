8b9a0b102841a497b5935cf60eb7d1ada993d3dd
Diego Mais <diegomais@live.com>
2019-10-13 19:10:05 +0200

    Add get list of devs from DB feature
    - This feature provides user to get the list of devs from DB.
    
    On branch master
    Changes to be committed:
    	modified:   frontend/src/pages/Main.js

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-
+import api from '../services/api';
 import './Main.css';
 
 import logo from '../assets/logo.svg';
@@ -8,106 +8,45 @@ import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        },
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
   return (
     <div className="main-container">
       <img src={logo} alt="TinDev" />
       <ul>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/40746974?s=460&v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Mais</strong>
-            <p>
-              Full Stack Developer: 5+ years of experience in Software
-              Development (mainly JavaScript). Currently interested in Node.js,
-              Angular, ReactJS and React Native.
-            </p>
-          </footer>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt={user.name} />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
 
-          <div className="buttons">
-            <button type="button" onClick={() => {}}>
-              <img src={dislike} alt="Dislike" />
-            </button>
+            <div className="buttons">
+              <button type="button" onClick={() => {}}>
+                <img src={dislike} alt="Dislike" />
+              </button>
 
-            <button type="button" onClick={() => {}}>
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/40746974?s=460&v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Mais</strong>
-            <p>
-              Full Stack Developer: 5+ years of experience in Software
-              Development (mainly JavaScript). Currently interested in Node.js,
-              Angular, ReactJS and React Native.
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button" onClick={() => {}}>
-              <img src={dislike} alt="Dislike" />
-            </button>
-
-            <button type="button" onClick={() => {}}>
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/40746974?s=460&v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Mais</strong>
-            <p>
-              Full Stack Developer: 5+ years of experience in Software
-              Development (mainly JavaScript). Currently interested in Node.js,
-              Angular, ReactJS and React Native.
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button" onClick={() => {}}>
-              <img src={dislike} alt="Dislike" />
-            </button>
-
-            <button type="button" onClick={() => {}}>
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-        <li>
-          <img
-            src="https://avatars0.githubusercontent.com/u/40746974?s=460&v=4"
-            alt=""
-          />
-          <footer>
-            <strong>Diego Mais</strong>
-            <p>
-              Full Stack Developer: 5+ years of experience in Software
-              Development (mainly JavaScript). Currently interested in Node.js,
-              Angular, ReactJS and React Native.
-            </p>
-          </footer>
-
-          <div className="buttons">
-            <button type="button" onClick={() => {}}>
-              <img src={dislike} alt="Dislike" />
-            </button>
-
-            <button type="button" onClick={() => {}}>
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
+              <button type="button" onClick={() => {}}>
+                <img src={like} alt="Like" />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
