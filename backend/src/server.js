bb21e455841e0b00d51acf073220853380a3afcd
Diego Mais <diegomais.dev@gmail.com>
2020-09-04 22:48:34 -0300

    🔒 Add MongoDB URI to environment variable

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
 const routes = require('./routes');
-
+const PORT = process.env.PORT || 3333;
 const server = express();
 
-mongoose.connect(
-  'mongodb+srv://tindev:MitB1LLrj9D5BG2y@tindev-kxbik.mongodb.net/tindev?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 server.use(cors());
 server.use(express.json());
 server.use(routes);
-
-server.listen(3333);
+server.listen(PORT);
