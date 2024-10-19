e292c73336583e492e732a603de1c3c07573181f
Diego Mais <diegomais.pro@gmail.com>
2024-10-18 21:50:45 -0300

    chore(backend): remove Sentry

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,6 @@
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const Sentry = require('@sentry/node');
-const Tracing = require('@sentry/tracing');
 const routes = require('./routes');
 
 const app = express();
@@ -10,17 +8,6 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: { origin: '*', methods: ['GET', 'POST'] },
 });
-
-if (process.env.NODE_ENV !== 'production') {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Tracing.Integrations.Express({ server }),
-    ],
-    tracesSampleRate: 1.0,
-  });
-}
 
 const connectedUsers = {};
 io.on('connection', socket => {
@@ -41,16 +28,6 @@ app.use(function onConnection(req, res, next) {
 
 app.use(cors());
 app.use(express.json());
-
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.tracingHandler());
-
 app.use(routes);
-
-app.use(Sentry.Handlers.errorHandler());
-app.use(function onError(err, req, res, next) {
-  res.statusCode = 500;
-  res.end(res.sentry + '\n');
-});
 
 module.exports = app;
